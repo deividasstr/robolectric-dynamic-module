812e6ffb036622fe76b9e8a1c10515b81b0298cd
David <deividas.strioga@gmail.com>
2019-06-13 13:42:57 +0200

    specify application in the test

--- dynamic_feature/src/test/java/com/example/dynamic_feature/ActivityTest.kt
+++ dynamic_feature/src/test/java/com/example/dynamic_feature/ActivityTest.kt
@@ -2,15 +2,15 @@ package com.example.dynamic_feature
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.example.dynamicrobolectrictest.DynamicApp
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.example.dynamicrobolectrictest.R as baseR
-
-
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(application = DynamicApp::class)
 class ActivityTest {
 
     @get:Rule
